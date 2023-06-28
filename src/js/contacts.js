46ca70182be72df47effa9ae77f54ded2898cfbd
TudorModVis <131814733+TudorModVis@users.noreply.github.com>
2023-06-28 14:28:50 +0300

    Add files via upload

--- src/js/contacts.js
+++ src/js/contacts.js
@@ -1,15 +1,15 @@
-const serviceButtons = document.querySelectorAll('.service');
+const servicesButtons = document.querySelectorAll('.service');
 
 function activateService(event) {
-    console.log(event.currentTarget)
     event.currentTarget.classList.toggle('active');
 }
 
-serviceButtons.forEach(button => {
+servicesButtons.forEach(button => {
     button.addEventListener('click', activateService);
 });
 
 // -------- Menu -------- //
+
 const header = document.querySelector('header');
 function openMenu() {
     menu.classList.toggle('active');
@@ -44,31 +44,3 @@ function openMenu() {
 }
 
 menuButton.addEventListener('click', openMenu);
-
-// Initialize and add the map
-let map;
-
-async function initMap() {
-  // The location of Uluru
-  const position = { lat: -25.344, lng: 131.031 };
-  // Request needed libraries.
-  //@ts-ignore
-  const { Map } = await google.maps.importLibrary("maps");
-  const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
-
-  // The map, centered at Uluru
-  map = new Map(document.getElementById("map"), {
-    zoom: 4,
-    center: position,
-    mapId: "DEMO_MAP_ID",
-  });
-
-  // The marker, positioned at Uluru
-  const marker = new AdvancedMarkerElement({
-    map: map,
-    position: position,
-    title: "Uluru",
-  });
-}
-
-initMap();
